03bf4c6743603da95ba26ad24fbe5e7488f4afda
Mayank Jha <jhamayank159@gmail.com>
2020-03-03 22:56:49 +0530

    Controlling LED Via Bluetooth

--- BattiScript.ino
+++ BattiScript.ino
@@ -1,11 +1,12 @@
-String incoming;
-const int battipin=13;
+//An LED connected To Pin 13 turns on and off according to a serial command recieved, I am using a HC-05 Bluetooth Module for Serial Communication. 
+String incoming; //declare incoming variable
+const int battipin=13; //set pinmode for LED
 void setup() 
 {
-  pinMode(battipin,OUTPUT);
-  digitalWrite(battipin,LOW);
-  Serial.begin(9600);
-  Serial.print("Kya Hukum Mere Aaka");
+  pinMode(battipin,OUTPUT); //set pin to output
+  digitalWrite(battipin,LOW); //remove voltage from pin
+  Serial.begin(9600); //set serial baud rate
+  Serial.print("Kya Hukum Mere Aaka"); 
 
 }
 
@@ -16,14 +17,14 @@ char recieved = (char)Serial.read();
 incoming += recieved;
 }
 Serial.println(incoming);
-if (incoming.equals("start")) 
+if (incoming.equals("start"))                     //turn led on
 {
 Serial.print("Batti On Hui Gawa");
 digitalWrite(battipin, HIGH);
 delay(500);
 incoming = "";
 }
-else if (incoming.equals("stop")) 
+else if (incoming.equals("stop"))               //turn led off
 {
 Serial.print("Batti Off Hui Gawa");
 digitalWrite(battipin, LOW); 
